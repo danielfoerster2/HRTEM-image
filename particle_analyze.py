0ce9354976a0bf58fba8b446670029fd41535803
vbarrere <victor.barrere45@gmail.com>
2026-03-04 10:47:19 +0100

    Add cluster separation and particle analysis scripts; update run.sh for new paths

---
+++ particle_analyze.py
@@ -0,0 +1,96 @@
+from ovito.io import import_file, export_file
+from ovito.modifiers import *
+from ovito.data import *
+
+import os
+import numpy as np
+
+path_xyz = os.getenv("path_xyz")
+path_processed = os.getenv("path_processed")
+path_new_xyz = os.getenv("path_new_xyz")
+
+files = os.listdir(path_new_xyz)
+for xyz_file in files:
+
+    i_sim = xyz_file.split(".")[0]
+    path_file = os.path.join(path_new_xyz, xyz_file)
+
+    with open(path_xyz + "d.dat", "r") as file:
+        lines = file.readlines()
+        i = 0
+        res = False
+        while not res and i < len(lines):
+            line = lines[i].split()
+            if line[0] == i_sim.split("_")[0]:
+                res = True
+            i += 1
+        n_steps = int(line[3])
+        initial_temperature = float(line[4])
+    node = import_file(path_file)
+        
+    surface_mod = ConstructSurfaceModifier()
+    node.modifiers.append(surface_mod)
+    cna_mod = CommonNeighborAnalysisModifier()
+    node.modifiers.append(cna_mod)
+    bond_angle_mod = BondAngleAnalysisModifier()
+    node.modifiers.append(bond_angle_mod)
+    csp_mod = CentroSymmetryModifier()
+    node.modifiers.append(csp_mod)
+    
+
+    data = node.compute()
+    pos = data.particle_properties.position.array
+    n_atoms = pos.shape[0]
+    epot = data.particle_properties['epot'].array
+    box = data.cell.matrix.diagonal()
+        
+
+    surface_area = node.output.attributes['ConstructSurfaceMesh.surface_area']
+    solid_volume = node.output.attributes['ConstructSurfaceMesh.solid_volume']
+        
+    cna_others = node.output.attributes['CommonNeighborAnalysis.counts.OTHER']
+    cna_fcc = node.output.attributes['CommonNeighborAnalysis.counts.FCC']
+    cna_hcp = node.output.attributes['CommonNeighborAnalysis.counts.HCP']
+    cna_bcc = node.output.attributes['CommonNeighborAnalysis.counts.BCC']
+    cna_ico = node.output.attributes['CommonNeighborAnalysis.counts.ICO']
+        
+        
+    bond_angle_others = node.output.attributes['BondAngleAnalysis.counts.OTHER']
+    bond_angle_fcc = node.output.attributes['BondAngleAnalysis.counts.FCC']
+    bond_angle_hcp = node.output.attributes['BondAngleAnalysis.counts.HCP']
+    bond_angle_bcc = node.output.attributes['BondAngleAnalysis.counts.BCC']
+    bond_angle_ico = node.output.attributes['BondAngleAnalysis.counts.ICO']
+
+    csp = np.mean(node.output.particle_properties['Centrosymmetry'].array)
+
+    pos_atm1 = pos[data.particle_properties.particle_type.array == 1]
+    pos_atm2 = pos[data.particle_properties.particle_type.array == 2]
+
+    nat1 = pos_atm1.shape[0]
+    nat2 = pos_atm2.shape[0]
+        
+    if nat1 != 0:
+        r_cm1 = np.sum(pos_atm1, axis=0) / nat1
+    else:
+        r_cm1 = np.array([np.nan, np.nan, np.nan])
+    if nat2 != 0:
+        r_cm2 = np.sum(pos_atm2, axis=0) / nat2
+    else:
+        r_cm2 = np.array([np.nan, np.nan, np.nan])
+    r_cm = np.sum(pos, axis=0) / n_atoms
+    
+    d_com = np.linalg.norm(r_cm1 - r_cm2)
+
+    gyration_radius = np.sqrt(np.sum(np.linalg.norm(pos, axis=1)**2) / n_atoms)
+
+    radius_limit = .8*gyration_radius
+    mask_out_atm1 = np.linalg.norm(pos_atm1, axis=1) > radius_limit
+    mask_out_atm2 = np.linalg.norm(pos_atm2, axis=1) > radius_limit
+    
+    nat1_out = np.sum(mask_out_atm1)
+    nat2_out = np.sum(mask_out_atm2)
+    nat1_in = np.sum(~mask_out_atm1)
+    nat2_in = np.sum(~mask_out_atm2)
+    
+    with open(os.path.join(path_processed, "data.dat"), "a") as f:
+        print(i_sim, n_atoms, nat1, nat2, n_steps, initial_temperature, np.sum(epot), surface_area, solid_volume, cna_others, cna_fcc, cna_hcp, cna_bcc, cna_ico, bond_angle_others, bond_angle_fcc, bond_angle_hcp, bond_angle_bcc, bond_angle_ico, d_com, gyration_radius, nat1_out, nat2_out, nat1_in, nat2_in, r_cm[0], r_cm[1], r_cm[2], r_cm1[0], r_cm1[1], r_cm1[2], r_cm2[0], r_cm2[1], r_cm2[2], csp, sep="\t", file=f)
--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 export path_xyz="/home/victor.barrere@crmd.cnrs-orleans.fr/Documents/Data/Data_xyz/"
-export path_processed="/home/victor.barrere@crmd.cnrs-orleans.fr/Documents/Data/Data_processed/"
+export path_processed="/home/victor.barrere@crmd.cnrs-orleans.fr/Documents/Data/Data_processed_test/"
 export path_new_xyz=$path_processed"XYZ/"
+
+mkdir -p $path_new_xyz
+rm -f $path_processed/data.dat
+
+$ovitos cluster_separation.py
+$ovitos particle_analyze.py
+
